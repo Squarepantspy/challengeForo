05b66276c7aac3e8ecbcb00c900ea849d91a0828
Squarepantspy <pgsantax@gmail.com>
2024-07-16 20:35:08 -0400

    se valida la actualizacion de topicos

--- src/main/java/com/aluracursos/challengeforo/domain/topico/DatosActualizarTopico.java
+++ src/main/java/com/aluracursos/challengeforo/domain/topico/DatosActualizarTopico.java
@@ -1,8 +1,14 @@
 package com.aluracursos.challengeforo.domain.topico;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record DatosActualizarTopico(
+        @NotNull
         Long id,
+        @NotBlank
         String mensaje,
+        @NotBlank
         String titulo
 ) {
 }
